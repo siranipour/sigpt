d4bf275aea16ed3bc58b289776b184799dda3c8a
siranipour <s.iranipour@gmail.com>
2024-08-17 17:05:01 +0100

    Add tests for API

---
+++ sigpt/server/tests/test_api.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+
+from sigpt.server import serve
+
+BASE_PROMPT = "Hello, I am a language model and"
+
+def test_api():
+    client = TestClient(serve.app)
+
+    response = client.get(f"/sigpt?prompt={BASE_PROMPT}")
+    assert response.status_code == 200
+    payload = response.json()
+    assert len(payload) == 1
+    assert not (payload[0].keys() ^ set(("input",  "output")))
+
+    n_batches = 3
+    response_multiple_batches = client.get(
+        f"/sigpt?prompt={BASE_PROMPT}&batches={n_batches}"
+    )
+    assert len(response_multiple_batches.json()) == n_batches
+    response_exceeds_max_batch = client.get(
+        f"/sigpt?prompt={BASE_PROMPT}&batches={serve.MAX_BATCHES + 1}"
+    )
+    assert response_exceeds_max_batch.status_code == 422
