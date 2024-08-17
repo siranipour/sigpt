f87110837bde2830dd9000be551aa047438f8c78
siranipour <s.iranipour@gmail.com>
2024-08-17 14:13:48 +0100

    Add API to query model

--- sigpt/server/serve.py
+++ sigpt/server/serve.py
@@ -1,10 +1,21 @@
-from sigpt.model import sample
+import dataclasses
+from typing import Optional
 
 from fastapi import FastAPI
+
+from sigpt.model import architecture, sample
+
+@dataclasses.dataclass
+class ModelResponse:
+    input: str
+    output: str
 
 app = FastAPI()
 
 @app.get("/sigpt/")
-def generate_tokens():
-    sample.hello
-    return {"Hello": "There"}
+def generate_tokens(
+    prompt: str, batches: Optional[int]=1, max_len: Optional[int]=20,
+) -> list[ModelResponse]:
+    mdl = architecture.Transformer(architecture.GPTConfig)
+    generated = sample.generate(mdl, prompt, batches, max_len)
+    return [ModelResponse(prompt, gen.lstrip(prompt)) for gen in generated]
