05092944efa5b8b88d77f8cf6fbdd2445a20ce0e
siranipour <s.iranipour@gmail.com>
2024-08-17 15:46:42 +0100

    Handle API requests

--- sigpt/server/serve.py
+++ sigpt/server/serve.py
@@ -1,9 +1,13 @@
 import dataclasses
 from typing import Optional
 
-from fastapi import FastAPI
+from better_profanity import profanity
+from fastapi import FastAPI, HTTPException
 
 from sigpt.model import architecture, sample
+
+MAX_BATCHES = 10
+MAX_GEN_LEN = 100
 
 @dataclasses.dataclass
 class ModelResponse:
@@ -16,6 +20,24 @@ app = FastAPI()
 def generate_tokens(
     prompt: str, batches: Optional[int]=1, max_len: Optional[int]=20,
 ) -> list[ModelResponse]:
+    check_request(prompt, batches, max_len)
     mdl = architecture.Transformer(architecture.GPTConfig)
     generated = sample.generate(mdl, prompt, batches, max_len)
     return [ModelResponse(prompt, gen.lstrip(prompt)) for gen in generated]
+
+def check_request(prompt: str, batches: int, max_len: int) -> None:
+    if max_len > MAX_GEN_LEN:
+        raise HTTPException(
+            status_code=422,
+            detail=f"Max requested sequence generation length cannot exceed {MAX_GEN_LEN}",
+        )
+    if batches > MAX_BATCHES:
+        raise HTTPException(
+            status_code=422,
+            detail=f"Max requested batches cannot exceed {MAX_BATCHES}",
+        )
+    if profanity.contains_profanity(prompt):
+        raise HTTPException(
+            status_code=422,
+            detail="Input prompt is not valid",
+        )
