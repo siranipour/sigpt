824241f801d9d0095a4a1794a4ae730f1248b23f
siranipour <s.iranipour@gmail.com>
2025-01-10 19:08:06 +0000

    Enumerate over dataset splits

--- src/sigpt/tests/test_data.py
+++ src/sigpt/tests/test_data.py
@@ -3,24 +3,26 @@ import tiktoken
 
 from sigpt import data
 
+@pytest.mark.parametrize("split", ["train", "validation"])
 @pytest.mark.parametrize("block_size", [1, 32, 1024])
 @pytest.mark.parametrize("batch_size", [1, 32])
-def test_block_size_dataset(block_size: int, batch_size: int):
+def test_block_size_dataset(split: str, block_size: int, batch_size: int):
     encoder = tiktoken.get_encoding("gpt2")
     dl = data.fetch_dataset_loader("train", encoder, batch_size, block_size)
     assert all(next(iter(dl)).shape == (batch_size, block_size))
 
     ddp1 = data.DDPConfig(1, 2)
     ddp2 = data.DDPConfig(2, 2)
-    dl1 = data.fetch_dataset_loader("train", encoder, batch_size, block_size, ddp=ddp1)
-    dl2 = data.fetch_dataset_loader("train", encoder, batch_size, block_size, ddp=ddp2)
+    dl1 = data.fetch_dataset_loader(split, encoder, batch_size, block_size, ddp=ddp1)
+    dl2 = data.fetch_dataset_loader(split, encoder, batch_size, block_size, ddp=ddp2)
     assert not (next(iter(dl1)) == (next(iter(dl2)))).any()
 
 
+@pytest.mark.parametrize("split", ["train", "validation"])
 @pytest.mark.parametrize("block_size", [1, 32, 1024])
-def test_block_size_dataset(block_size: int):
+def test_block_size_dataset(split: str, block_size: int):
     encoder = tiktoken.get_encoding("gpt2")
-    ds = data.BlockSizedDataset(block_size, "train", encoder)
+    ds = data.BlockSizedDataset(block_size, split, encoder)
     assert len(next(iter(ds))) == block_size
 
 
