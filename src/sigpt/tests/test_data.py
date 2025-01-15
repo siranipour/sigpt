4294b2a20abfe02a5e8623d02fea206f0eebf621
siranipour <s.iranipour@gmail.com>
2025-01-15 17:50:52 +0000

    Fix DataLoader shape comparison
    And fix DDPConfig spec

--- src/sigpt/tests/test_data.py
+++ src/sigpt/tests/test_data.py
@@ -9,14 +9,14 @@ from sigpt import data
 @pytest.mark.parametrize("batch_size", [1, 32])
 def test_block_size_dataloader(split: str, block_size: int, batch_size: int):
     encoder = tiktoken.get_encoding("gpt2")
-    dl = data.fetch_dataset_loader("train", encoder, batch_size, block_size)
-    assert all(next(iter(dl)).shape == (batch_size, block_size))
+    dl = data.fetch_dataset_loader(split, encoder, batch_size, block_size)
+    assert next(iter(dl)).shape == (batch_size, block_size)
 
-    ddp1 = data.DDPConfig(1, 1, 2)
-    ddp2 = data.DDPConfig(1, 2, 2)
+    ddp1 = data.DDPConfig(0, 0, 2)
+    ddp2 = data.DDPConfig(1, 1, 2)
     dl1 = data.fetch_dataset_loader(split, encoder, batch_size, block_size, ddp=ddp1)
     dl2 = data.fetch_dataset_loader(split, encoder, batch_size, block_size, ddp=ddp2)
-    assert not (next(iter(dl1)) == (next(iter(dl2)))).any()
+    assert (next(iter(dl1)) != (next(iter(dl2)))).any()
 
 
 @pytest.mark.parametrize("split", ["train", "validation"])
