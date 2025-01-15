63d073e004d3c6e5313bfe5892e31f45bc70327e
siranipour <s.iranipour@gmail.com>
2025-01-15 12:26:30 +0000

    Correct test name

--- src/sigpt/tests/test_data.py
+++ src/sigpt/tests/test_data.py
@@ -7,7 +7,7 @@ from sigpt import data
 @pytest.mark.parametrize("split", ["train", "validation"])
 @pytest.mark.parametrize("block_size", [1, 32, 1024])
 @pytest.mark.parametrize("batch_size", [1, 32])
-def test_block_size_dataset(split: str, block_size: int, batch_size: int):
+def test_block_size_dataloader(split: str, block_size: int, batch_size: int):
     encoder = tiktoken.get_encoding("gpt2")
     dl = data.fetch_dataset_loader("train", encoder, batch_size, block_size)
     assert all(next(iter(dl)).shape == (batch_size, block_size))
